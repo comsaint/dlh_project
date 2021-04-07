feb0590a88d2daf83dcbce422f703612e1b5b678
Long <long.pun@sands.com.mo>
2021-04-07 14:24:52 +0800

    add support for resnext50

--- src/model.py
+++ src/model.py
@@ -75,8 +75,18 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
         model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
         # Handle the primary net
         num_ftrs = model_ft.fc.in_features
-        model_ft.fc = nn.Linear(num_ftrs,num_classes)
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
         input_size = 299
+
+    elif model_name == "resnext50":
+        """
+        ResNeXt-50
+        """
+        model_ft = models.resnext50_32x4d(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
 
     else:
         raise Exception(f"Invalid model name '{model_name}'")
